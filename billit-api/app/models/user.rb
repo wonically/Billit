a0efcf6e113b16d6537006de7550328682281678
Wony Nguyen <88579394+wonically@users.noreply.github.com>
2025-08-06 20:38:26 +0700

    Add role enum to User

--- billit-api/app/models/user.rb
+++ billit-api/app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: { admin: 0, client: 1 }
 end
