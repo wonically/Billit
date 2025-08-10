9f93db7d0088fb4c7b2bb39a72257ebca857015d
Wony Nguyen <88579394+wonically@users.noreply.github.com>
2025-08-10 13:12:30 +0700

    Scheduling with Sidekiq + Cron

---
+++ billit-api/config/initializers/sidekiq.rb
@@ -0,0 +1,8 @@
+require 'sidekiq'
+require 'sidekiq-cron'
+
+schedule_file = Rails.root.join('config', 'schedule.yml')
+
+if File.exist?(schedule_file)
+  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+end
